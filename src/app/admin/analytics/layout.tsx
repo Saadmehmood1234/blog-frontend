d4e358a473858d017dde8bf38ff76bf3d021eabd
Saad <mehmoodsaad347@gmail.com>
2026-01-26 01:08:29 +0530

    added

--- src/app/admin/analytics/layout.tsx
+++ src/app/admin/analytics/layout.tsx
@@ -7,6 +7,8 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const user = await getCookies();
+    console.log("Usser",user)
+
   if (!user) {
     redirect("/admin/auth/signin");
   }
--- src/app/admin/auth/layout.tsx
+++ src/app/admin/auth/layout.tsx
@@ -7,6 +7,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const user = await getCookies();
+  console.log("Usser",user)
   if (user) {
     redirect("/admin/analytics");
   }
